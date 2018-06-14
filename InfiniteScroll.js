6c20b1df27756ecbc51c9ee43bdde2d4d69351d0
Angela Park <angela8592@yahoo.com>
2018-06-14 11:59:27 -0700

    add InfiniteScroll.js

---
+++ InfiniteScroll.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class InfiniteScroll extends React.Component {
+  callback = entries => {
+    let isIntersecting = false;
+
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        isIntersecting = true;
+        break;
+      }
+    }
+
+    if (isIntersecting) {
+      this.props.onVisible();
+    }
+  };
+
+  ref = React.createRef();
+
+  componentDidMount() {
+    this.observer = new IntersectionObserver(this.callback, {
+      rootMargin: "50px"
+    });
+
+    this.observer.observe(this.ref.current);
+  }
+
+  componentWillUnmount() {
+    this.observer.unobserve(this.ref.current);
+  }
+
+  render() {
+    return <div ref={this.ref} />;
+  }
+}
+
+export default InfiniteScroll;
